231feb1cc3c2e173f0b6d1d9b1002fc63a3f206b
mattiadane <mattia.danese04@gmail.com>
2026-04-30 14:03:14 +0200

    modificato bottoni in checkbox

--- front-end/home_page.py
+++ front-end/home_page.py
@@ -2,7 +2,14 @@ import streamlit as st
 
 if "state" not in st.session_state:
     st.session_state.state = {
-        "surgical_op": False
+        "nephro" : False,
+        "spesi": False,
+        "diuretic" : False,
+        "venpres" : None,
+        "conmed": False,
+        "surgical_op": False,
+        "sispress" : None,
+        "antihypertensive" : False,
     }
 
 # funzione per aprire il file css
@@ -20,50 +27,51 @@ st.set_page_config(page_title="Kidney Injury", page_icon="🧬")
 st.title("Prediction of Kidney Injury")
 
 
-button_nephro = st.button("Nephrotoxicity drugs")
+check_nephro = st.checkbox("Nephrotoxicity drugs")
+if check_nephro:
+    st.session_state.state["nephro"] = True
+
+
+check_sepsi = st.checkbox("Sepsi")
+if check_sepsi:
+    st.session_state.state["sepsi"] = True
+
+
+check_diuretic = st.checkbox("Diuretic drugs")
+if check_diuretic:
+    st.session_state.state["diuretic"] = True
+
+
+check_conmed = st.checkbox("Contrast Medium")
+if check_conmed:
+    st.session_state.state["conmed"] = True
 
 
 
-if button_nephro:
-    st.write("Nephrotoxicity drugs")
-    # richiamare query per selezionare farmaci nefrotossici
+enable_slider1 = st.checkbox("Enable parameter venouse pressoure")
 
-button_sepsi = st.button("Sepsi")
-if button_sepsi:
-    st.write("Sepsi")
-    # richiamare query per selezionare sepsi
-
-
-button_diuretic = st.button("Diuretic drugs")
-if button_diuretic:
-    st.write("Diuretic drugs")
-    # richiamare query per selezionare farmaci direttici
-
-
-min_central_ven_pres= st.slider("Select min value ",-10,20)
-st.write("hai selezionato il valore ", min_central_ven_pres)
-
-button_conmed = st.button("Contrast Medium")
-if button_conmed:
-    st.write("Contrast Medium")
-    # richiamare query per selezionare constrasto medio
+if enable_slider1:
+    st.session_state.state["venpres"] =  st.slider("Select min value ",-10,20)
+else:
+    st.session_state.state["venpres"] = None
 
 
 check_surgical_op = st.checkbox("surgical operation")
 if check_surgical_op:
-    st.write("Surgical operation is clicked")
     st.session_state.state["surgical_op"] = True
 
 
-min_sis_pres= st.slider("Select min value ",50,160)
-st.write("hai selezionato il valore ", min_sis_pres)
 
+enable_slider2 = st.checkbox("Enable parameter sistolic pressoure")
 
-button_antihypertensive = st.button("Antihypertensive drugs")
-if button_antihypertensive:
-    st.write("Antihypertensive drugs")
-    # richiamare query per selezionare farmaci antiipertensività
+if enable_slider2 :
+    st.session_state.state["sispress"] =  st.slider("Select min value ",50,160)
+else:
+    st.session_state.state["sispress"] = None
 
+check_antihypertensive = st.checkbox("Antihypertensive drugs")
+if check_antihypertensive:
+    st.session_state.state["antihypertensive"] = True
 
 
 button_opendialog = st.button("Choose Framework")
@@ -71,14 +79,9 @@ button_opendialog = st.button("Choose Framework")
 
 @st.dialog("Select framework")
 def select_framework():
-    messaa = " Hai selezionato surgical op?"
-    if st.session_state.state["surgical_op"]:
-        messaa += " Si"
-    else :
-        messaa += " No"
 
-    st.write(messaa)
 
+    st.write("hai selezionato  : ", st.session_state.state)
     choice = st.radio(
         "Seleziona un'framework:",
         ["framework 1", "Framework 2"],
