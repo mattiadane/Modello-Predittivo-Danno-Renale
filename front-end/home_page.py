9c9b4017759de9a335b670a1ebe2f548ddfee782
Sebastiano Mazzaro <smazzaro.02@gmail.com>
2026-05-06 17:38:12 +0200

    Prove css

--- front-end/home_page.py
+++ front-end/home_page.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="Kidney Injury", page_icon="🧬")
+
 if "state" not in st.session_state:
     st.session_state.state = {
         "nephro" : False,
@@ -20,66 +22,51 @@ def local_css(file_name):
 # Richiama la funzione passando il nome del file
 local_css("front-end/style/style.css")
 
-st.set_page_config(page_title="Kidney Injury", page_icon="🧬")
-
-
-
 st.title("Prediction of Kidney Injury")
 
+# ===== FARMACI =====
+with st.container(border=True):
+    st.markdown("### Farmaci")
+    st.session_state.state["nephro"] = st.checkbox("Nephrotoxicity drugs")
+    st.session_state.state["diuretic"] = st.checkbox("Diuretic drugs")
+    st.session_state.state["antihypertensive"] = st.checkbox("Antihypertensive drugs")
 
-check_nephro = st.checkbox("Nephrotoxicity drugs")
-if check_nephro:
-    st.session_state.state["nephro"] = True
+# ===== PRESSIONI =====
+with st.container(border=True):
+    st.markdown("### Pressioni")
 
+    if st.checkbox("Enable venous pressure"):
+        st.session_state.state["venpres"] = st.slider("Venous pressure", -10, 20)
+    else:
+        st.session_state.state["venpres"] = None
+
+    if st.checkbox("Enable systolic pressure"):
+        st.session_state.state["sispress"] = st.slider("Systolic pressure", 50, 160)
+    else:
+        st.session_state.state["sispress"] = None
+
+st.write("")
 
 check_sepsi = st.checkbox("Sepsi")
 if check_sepsi:
     st.session_state.state["sepsi"] = True
 
-
-check_diuretic = st.checkbox("Diuretic drugs")
-if check_diuretic:
-    st.session_state.state["diuretic"] = True
-
+st.write("")
 
 check_conmed = st.checkbox("Contrast Medium")
 if check_conmed:
     st.session_state.state["conmed"] = True
 
+st.write("")
 
-
-enable_slider1 = st.checkbox("Enable parameter venouse pressoure")
-
-if enable_slider1:
-    st.session_state.state["venpres"] =  st.slider("Select min value ",-10,20)
-else:
-    st.session_state.state["venpres"] = None
-
-
-check_surgical_op = st.checkbox("surgical operation")
+check_surgical_op = st.checkbox("Surgical Operation")
 if check_surgical_op:
     st.session_state.state["surgical_op"] = True
 
-
-
-enable_slider2 = st.checkbox("Enable parameter sistolic pressoure")
-
-if enable_slider2 :
-    st.session_state.state["sispress"] =  st.slider("Select min value ",50,160)
-else:
-    st.session_state.state["sispress"] = None
-
-check_antihypertensive = st.checkbox("Antihypertensive drugs")
-if check_antihypertensive:
-    st.session_state.state["antihypertensive"] = True
-
-
 button_opendialog = st.button("Choose Framework")
-
 
 @st.dialog("Select framework")
 def select_framework():
-
 
     st.write("hai selezionato  : ", st.session_state.state)
     choice = st.radio(
@@ -90,9 +77,7 @@ def select_framework():
     button_submit = st.button("Submit")
 
     if button_submit:
-
         st.write("hai selezionato " + choice )
-
 
 if button_opendialog:
     select_framework()
